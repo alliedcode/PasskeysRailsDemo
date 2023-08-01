8ab5c4ba1ad964b932965d07b1f073ba7d627464
Troy Anderson <troy@alliedcode.com>
2023-07-31 20:53:58 -0700

    Initial Commit

---
+++ PasskeysRailsDemo/PasskeysRailsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasskeysRailsDemoApp.swift
+//  PasskeysRailsDemo
+//
+//  Created by Troy Anderson on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct PasskeysRailsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
